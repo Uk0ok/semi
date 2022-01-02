514fd9cdcde97c9d3f1cd908e3613c53d5e30777
DESKTOP-CAR9PQK\user <chashu9886@naver.com>
2022-01-02 11:23:44 +0900

    write controller 수정

--- Recipetory/src/main/java/com/reci/chal/controller/ChalViewController.java
+++ Recipetory/src/main/java/com/reci/chal/controller/ChalViewController.java
@@ -15,4 +15,5 @@ public class ChalViewController extends HttpServlet{
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.getRequestDispatcher("/WEB-INF/views/challenge/challenge_view.jsp").forward(req, resp);
 	}
+	
 }
