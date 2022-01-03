5dfaf42e80d149722025628f753c283f75a63437
DESKTOP-VM093GL\hizoo <hizoo423@gmail.com>
2022-01-03 16:38:55 +0900

    xx

--- Recipetory/src/main/java/com/reci/join/controller/LoginController.java
+++ Recipetory/src/main/java/com/reci/join/controller/LoginController.java
@@ -12,10 +12,10 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginController extends HttpServlet{
 	
 	//로그인 화면 보여주기
-		@Override
-		protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-			req.getRequestDispatcher("/WEB-INF/views/join/login.jsp").forward(req, resp);
-		}
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("/WEB-INF/views/join/login.jsp").forward(req, resp);
+	}
 
 		//로그인 진행
 		@Override
